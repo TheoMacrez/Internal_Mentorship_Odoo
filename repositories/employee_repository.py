94903c2ef0e69454c9fe58692aec4505f392daac
Theo Macrez <macreztheo.prog@gmail.com>
2026-06-17 16:29:05 +0200

    [IMP][FIX] employee_repository : added find_by_email

--- repositories/employee_repository.py
+++ repositories/employee_repository.py
@@ -17,7 +17,24 @@ class EmployeeRepository:
                         birth_date
                     FROM employee
                     WHERE id_employee = %s
-                """, (employee_id,))
+                """, (employee_id))
+
+                return cur.fetchone()
+    
+    def find_by_email(self, email):
+        with get_connection() as conn:
+            with conn.cursor(row_factory=class_row(Employee)) as cur:
+                cur.execute("""
+                    SELECT
+                        id_employee,
+                        first_name,
+                        last_name,
+                        email,
+                        phone,
+                        birth_date
+                    FROM employee
+                    WHERE email = %s
+                """, (email))
 
                 return cur.fetchone()
     
@@ -32,7 +49,7 @@ class EmployeeRepository:
 
                 return cur.fetchone()
     
-    def update(self, first_name, last_name, email, phone, birth_date):
+    def update(self, first_name, last_name, email, phone, birth_date, employee_id):
         with get_connection() as conn:
             with conn.cursor(row_factory=class_row(Employee)) as cur:
                 cur.execute("""
@@ -44,7 +61,7 @@ class EmployeeRepository:
                         birth_date = %s
                     WHERE id_employee = %s
                     RETURNING id_employee, first_name, last_name, email, phone, birth_date;
-            """, (first_name, last_name, email, phone, birth_date))
+            """, (first_name, last_name, email, phone, birth_date, employee_id))
 
                 return cur.fetchone()
     
@@ -53,8 +70,11 @@ class EmployeeRepository:
             with conn.cursor(row_factory=class_row(Employee)) as cur:
                 cur.execute("""
                     DELETE FROM employee
-                        WHERE id_employee = %s
+                    WHERE id_employee = %s
+                    RETURNING id_employee, first_name, last_name, email, phone, birth_date;
                 """, (employee_id,))
             
-            #Check if the deletion has been done
-            return cur.fetchone() is not None
+                #Check if the deletion has been done
+                return cur.fetchone() is not None
+    
+
